048bffb8feb13c38aff563fc34143cc704ded671
0xlunar <20531468+0xlunar@users.noreply.github.com>
2023-05-21 00:03:29 +1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,167 @@
+use rand::Rng;
+use clearscreen;
+
+use crossterm::{
+    event::{read, KeyEvent, Event, KeyCode},
+};
+use std::{io, thread, time::Duration};
+
+fn main(){
+    let mut board = Board::new(16, 16);
+    board.start();
+}
+
+type Node<T> = Option<Box<T>>;
+#[derive(Debug, Clone, Copy)]
+enum Directions {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+#[derive(Debug)]
+struct Board {
+    height: i32,
+    width: i32,
+    fruit_pos: (i32, i32),
+    score: i32,
+    alive: bool,
+    direction: Directions,
+    snake: Snake,
+}
+#[derive(Debug, Clone)]
+struct Snake {
+    head: (i32, i32),
+    tail: Node<Snake>,
+}
+
+impl Snake {
+    fn new(x: i32, y: i32) -> Snake {
+        Snake { head: (x, y), tail: None }
+    }
+
+    pub fn eat_fruit(&mut self) {
+        match &mut self.tail {
+            None => self.tail = Some(Box::new(Snake::new(self.head.0, self.head.1))),
+            Some(snake) => snake.eat_fruit(),
+        }
+    }
+
+    pub fn move_direction(&mut self, direction: Directions) {
+        self.move_body(&direction);
+        match direction {
+            Directions::UP => self.head.1 -= 1,
+            Directions::DOWN => self.head.1 += 1,
+            Directions::LEFT => self.head.0 -= 1,
+            Directions::RIGHT => self.head.0 += 1
+        }
+    }
+
+    fn move_body(&mut self, direction: &Directions) {
+        match &mut self.tail {
+            Some(snake) => {
+                snake.head.0 = self.head.0;
+                snake.head.1 = self.head.1;
+                snake.move_body(direction)
+                // snake.move_direction(direction.to_owned());
+            },
+            None => (),
+        }
+    }
+
+    fn render(&self, grid: &mut[&mut[char]]) {
+        grid[self.head.0 as usize][self.head.1 as usize] = '#';
+        match &self.tail {
+            Some(snake) => snake.render(grid),
+            None => (),
+        }
+    }
+}
+
+impl Board {
+    pub fn new(width: i32, height: i32) -> Board {
+        let mut rng = rand::thread_rng();
+        let rand_x: i32 = (rng.gen::<f64>() * width as f64) as i32;
+        let rand_y: i32 = (rng.gen::<f64>() * height as f64) as i32;
+
+        Board { height, width, score: 0, fruit_pos: (rand_x, rand_y), alive: true, direction: Directions::RIGHT, snake: Snake::new(height / 2, width / 2) }
+    }
+
+    pub fn move_snake(&mut self, direction: Directions) {
+        if !self.alive {
+            return;
+        }
+        self.snake.move_direction(direction);
+        let head = &self.snake.head;
+        if head.0 < 0 || head.0 > (self.width - 1).try_into().unwrap() || head.1 < 0 || head.1 > (self.height - 1).try_into().unwrap() {
+            self.alive = false;
+        } else if head.0 == self.fruit_pos.0.try_into().unwrap() && head.1 == self.fruit_pos.1.try_into().unwrap() {
+            self.snake.eat_fruit();
+            self.spawn_fruit();
+        }
+    }
+
+    fn spawn_fruit(&mut self) {
+        let mut rng = rand::thread_rng();
+        let rand_x: i32 = (rng.gen::<f64>() * self.width as f64) as i32;
+        let rand_y: i32 = (rng.gen::<f64>() * self.height as f64) as i32;
+
+        self.fruit_pos = (rand_x, rand_y);
+    }
+
+    fn render(&self) {
+        clearscreen::clear().expect("failed to clear");
+        
+        let mut grid = vec!['*'; (self.width * self.height).try_into().unwrap()];
+        let mut grid: Vec<_> = grid.as_mut_slice().chunks_mut((self.width).try_into().unwrap()).collect();
+        let grid = grid.as_mut_slice();
+
+        grid[self.fruit_pos.0 as usize][self.fruit_pos.1 as usize] = '@';
+        self.snake.render(grid);
+        
+
+        let mut builder = String::new();
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
+                builder.push(grid[j][i]);
+            }
+            builder.push('\n');
+        }
+        println!("X: {}, Y: {}", self.snake.head.0, self.snake.head.1);
+        println!("{}", builder);
+        thread::sleep(Duration::from_millis(100));
+    }
+
+    pub fn detect_input(&mut self) {
+        let event = read();
+        match event {
+            Ok(e) => match e {
+                Event::Key(KeyEvent { code, ..}) => {
+                    match code {
+                        KeyCode::Up => self.direction = Directions::UP,
+                        KeyCode::Down => self.direction = Directions::DOWN,
+                        KeyCode::Left =>  self.direction = Directions::LEFT,
+                        KeyCode::Right =>  self.direction = Directions::RIGHT,
+                        KeyCode::Esc => self.alive = false,
+                        _ => ()
+                    }
+                },
+                _ => ()
+            },
+            _ => ()
+        }
+        
+        println!("{:?}",  self.direction);
+    }
+
+    pub fn start(&mut self) {
+        while self.alive {
+            self.detect_input();
+            self.move_snake(self.direction);
+            self.render();
+        }
+    }
+
+}
+
+
